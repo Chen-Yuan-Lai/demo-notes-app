985557d634aafd1aecbf04d1d0a0f949305d809a
Cheng-Yuan-Lai <a186235@g,ail.com>
2024-02-28 13:05:36 +0800

    Adding the API

---
+++ packages/functions/src/create.ts
@@ -0,0 +1,34 @@
+import * as uuid from "uuid";
+import { Table } from "sst/node/table";
+import handler from "@notes/core/handler";
+import dynamoDb from "@notes/core/dynamodb";
+
+const dynamoDB = new AWS.DynamoDB.DocumentClient();
+
+export const main = handler(async (event) => {
+  let data = {
+    content: "",
+    attachment: "",
+  };
+
+  if (event.body !== null) {
+    // Request body is passed in as a JSON encoded string in 'event.body'
+    data = JSON.parse(event.body);
+  }
+
+  const params = {
+    TableName: Table.Notes.tableName,
+    Item: {
+      // The attributes of the item to be created
+      userId: "123", // The id of the author
+      noteId: uuid.v1(), // A unique uuid
+      content: data.content, // Parsed from request body
+      attachment: data.attachment, // Parsed from request body
+      createAt: Date.now(), // Current Unix timestamp
+    },
+  };
+
+  await dynamoDb.put(params);
+
+  return JSON.stringify(params.Item);
+});
