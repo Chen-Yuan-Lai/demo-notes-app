0dac4c39679b8df499b96a9dd42ca91bea7fce95
Cheng-Yuan-Lai <a186235@g,ail.com>
2024-02-28 23:07:03 +0800

    Setting up our React app

--- packages/frontend/src/main.tsx
+++ packages/frontend/src/main.tsx
@@ -8,25 +8,25 @@ import "./index.css";
 
 Amplify.configure({
   Auth: {
-    mandatorySignIn: true,
-    region: config.cognito.REGION,
-    userPoolId: config.cognito.USER_POOL_ID,
-    identityPoolId: config.cognito.IDENTITY_POOL_ID,
-    userPoolWebClientId: config.cognito.APP_CLIENT_ID,
+    Cognito: {
+      userPoolId: config.cognito.USER_POOL_ID,
+      identityPoolId: config.cognito.IDENTITY_POOL_ID,
+      userPoolClientId: config.cognito.APP_CLIENT_ID,
+    },
   },
   Storage: {
-    region: config.s3.REGION,
-    bucket: config.s3.BUCKET,
-    identityPoolId: config.cognito.IDENTITY_POOL_ID,
+    S3: {
+      region: config.cognito.REGION,
+      bucket: config.s3.BUCKET,
+    },
   },
   API: {
-    endpoints: [
-      {
-        name: "notes",
+    REST: {
+      notes: {
         endpoint: config.apiGateway.URL,
         region: config.apiGateway.REGION,
       },
-    ],
+    },
   },
 });
 
